05fc816cfe633cae0befe4fb300aded7adbcfff8
Ben Bowes <ben.bowes@alintaenergy.com.au>
2020-06-10 02:20:17 +1000

    feat: include styleguide in gitignore

--- src/docs/StyleguidistWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import './StyleguidistStyle.css';
-import '../ReactResponsiveSelect.css';
-
-const StyleguidistWrapper: React.FC = ({ children }) => <>{children}</>;
-
-export default StyleguidistWrapper;
---
+++ src/docs/Wrapper.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import './StyleguidistStyle.css';
+import '../ReactResponsiveSelect.css';
+
+const Wrapper: React.FC = ({ children }) => <>{children}</>;
+
+export default Wrapper;
